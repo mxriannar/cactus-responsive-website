280ff974f9aae3069e4890fb292e13948fa6a169
Marianna Rodrigues <mxriannar@gmail.com>
2024-06-23 20:03:12 -0300

    scroll reveal animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,3 +69,15 @@ const scrollActive = () => {
 window.addEventListener('scroll', scrollActive)
 
 // scroll reveal animation
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2500,
+    delay: 300,
+    // reset: true, // animations repeat
+})
+
+sr.reveal(`.home__img, .new__data, .care__img, .contact__content, .footer`)
+sr.reveal(`.home__data, .care_list, .contact__img`, {delay: 500})
+sr.reveal(`.new__card`, {delay: 500, interval: 100})
+sr.reveal(`.shop__card`, {interval: 100})
