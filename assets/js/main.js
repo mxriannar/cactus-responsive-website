7ddeb905fc1cbc9ee98ba841d7c82779685e4095
Marianna Rodrigues <mxriannar@gmail.com>
2024-06-23 01:15:44 -0300

    active links

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,34 @@ const blurHeader = () => {
 window.addEventListener('scroll', blurHeader)
 
 // show scroll up
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // when the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 // scroll sections active link
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+            if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+                sectionClass.classList.add('active-link')
+            } else {
+                sectionClass.classList.remove('active-link')
+            }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
 
 // scroll reveal animation
